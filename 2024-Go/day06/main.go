d55e4627677e768268da28aa049da5608cd99b6a
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-12-07 14:20:10 +0000

    day06 not the prettiest of codes

--- 2024-Go/day06/main.go
+++ 2024-Go/day06/main.go
@@ -6,89 +6,156 @@ import (
 )
 
 const GUARD rune = '^'
-const OBSTACLE rune = '#'
-const VISITED rune = 'X'
 
-var DIRECTIONS = [][2]int{
-	{-1, 0},
-	{0, 1},
-	{1, 0},
-	{0, -1},
-}
-
-type Guard struct {
-	currDir int
-	l       int
-	c       int
-	steps   int
-}
-
-func initGuard(input [][]rune) Guard {
-	guard := Guard{
-		currDir: 0,
-		l:       0,
-		c:       0,
-		steps:   1,
-	}
-
-	for i, line := range input {
-		for j, ch := range line {
-			if ch == GUARD {
-				guard.l = i
-				guard.c = j
-				input[i][j] = VISITED
-				return guard
+func initializeGuardPosition(grid [][]rune, n, m int) (int, int) {
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if grid[i][j] == '^' {
+				return i, j
 			}
 		}
 	}
-	return guard
+	return -1, -1
 }
 
-func (g Guard) getForwardDir() [2]int {
-	return DIRECTIONS[g.currDir]
-}
+func collectSeen(grid [][]rune, ii, jj int, n, m int, dd [][]int) map[[2]int]struct{} {
+	dir := 0
+	seen := make(map[[2]int]struct{})
+	i, j := ii, jj
 
-func (g *Guard) turn() {
-	g.currDir = (g.currDir + 1) % len(DIRECTIONS)
-}
+	for {
+		seen[[2]int{i, j}] = struct{}{}
 
-// Returns false when dest is met
-func (g *Guard) patrol(input [][]rune) bool {
-	l := g.l + g.getForwardDir()[0]
-	c := g.c + g.getForwardDir()[1]
+		nextI := i + dd[dir][0]
+		nextJ := j + dd[dir][1]
 
-	// Turn in case of obstacle
-	if input[l][c] == OBSTACLE {
-		g.turn()
-		return true
+		if nextI < 0 || nextI >= n || nextJ < 0 || nextJ >= m {
+			break
+		}
+
+		if grid[nextI][nextJ] == '#' {
+			dir = (dir + 1) % 4
+		} else {
+			i, j = nextI, nextJ
+		}
 	}
 
-	// Move forward
-	g.l = l
-	g.c = c
+	return seen
+}
 
-	// Check if pos was previosly visited
-	if input[g.l][g.c] != VISITED {
-		g.steps++
-		input[g.l][g.c] = VISITED
+func willLoop(grid [][]rune, ii, jj, oi, oj int, n, m int, dd [][]int) bool {
+	if grid[oi][oj] == '#' {
+		return false
 	}
 
-	// false if end was reached true othewise
-	return l != 0 && l != len(input)-1 && c != 0 && c != len(input[0])-1
+	grid[oi][oj] = '#'
+	i, j := ii, jj
+	dir := 0
+	seen := make(map[[3]int]struct{})
+
+	for {
+		if _, exists := seen[[3]int{i, j, dir}]; exists {
+			grid[oi][oj] = '.'
+			return true
+		}
+		seen[[3]int{i, j, dir}] = struct{}{}
+
+		nextI := i + dd[dir][0]
+		nextJ := j + dd[dir][1]
+
+		if nextI < 0 || nextI >= n || nextJ < 0 || nextJ >= m {
+			grid[oi][oj] = '.'
+			return false
+		}
+
+		if grid[nextI][nextJ] == '#' {
+			dir = (dir + 1) % 4
+		} else {
+			i, j = nextI, nextJ
+		}
+	}
 }
 
 func part1(filename string) int {
-	input := lib.ParseCharMatrix(filename)
-	guard := initGuard(input)
+	grid := lib.ParseCharMatrix(filename)
 
-	for guard.patrol(input) {
+	n := len(grid)
+	m := len(grid[0])
+
+	var i, j int
+	found := false
+	for i = 0; i < n; i++ {
+		for j = 0; j < m; j++ {
+			if grid[i][j] == '^' {
+				found = true
+				break
+			}
+		}
+		if found {
+			break
+		}
 	}
 
-	return guard.steps
+	dir := 0
+	dd := [][]int{
+		{-1, 0}, // up
+		{0, 1},  // right
+		{1, 0},  // down
+		{0, -1}, // left
+	}
+
+	// Track visited cells
+	seen := make(map[[2]int]bool)
+
+	for {
+		seen[[2]int{i, j}] = true
+
+		nextI := i + dd[dir][0]
+		nextJ := j + dd[dir][1]
+
+		if nextI < 0 || nextI >= n || nextJ < 0 || nextJ >= m {
+			break
+		}
+
+		if grid[nextI][nextJ] == '#' {
+			dir = (dir + 1) % 4
+		} else {
+			i, j = nextI, nextJ
+		}
+	}
+
+	return len(seen)
 }
 
 func part2(filename string) int {
-	return -1
+	grid := lib.ParseCharMatrix(filename)
+	n := len(grid)
+	m := len(grid[0])
+
+	ii, jj := initializeGuardPosition(grid, n, m)
+	if ii == -1 || jj == -1 {
+		panic("Guard position not found")
+	}
+
+	dd := [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+
+	ogSeen := collectSeen(grid, ii, jj, n, m, dd)
+	ans := 0
+
+	for key := range ogSeen {
+		oi, oj := key[0], key[1]
+
+		// Skip the guard position
+		if oi == ii && oj == jj {
+			continue
+		}
+
+		if willLoop(grid, ii, jj, oi, oj, n, m, dd) {
+			ans++
+		}
+	}
+
+	return ans
 }
 
 func main() {
